f334c53d9764bcd2a377279200485fb7f018e257
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-11 14:36:17 +0100

    Updated

--- Installpackages.R
+++ Installpackages.R
@@ -2,4 +2,8 @@ install.packages("shiny")
 install.packages("tidyverse")
 install.packages("fivethirtyeight")
 install.packages("plotly")
-                 
+install.packages("dplyr")
+
+#Install Genome wide annotation for Human package
+source("https://bioconductor.org/biocLite.R")
+BiocInstaller::biocLite("org.Hs.eg.db")
