21d5bec23df8d90bea82685fc76ffd59f512af72
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-13 10:35:24 +0100

    Index column removed

--- Merge.py
+++ Merge.py
@@ -2,24 +2,25 @@
 import os, sys, csv, glob
 import pandas as pd
 
-#Add column headers "Genes", "above20x" and "average" to each file------------------------
+#Convert text files to csv files and add column headers "Genes", "above20x" and "average" to each file------------------------
 
 from pandas import read_csv
 
 import glob
-for file in glob.glob("/home/rduffin/Desktop/R_projects/*new.csv"):
-    df = read_csv(file, header=None, index_col=False)
+for file in glob.glob("/home/rduffin/Desktop/R_projects/*new.txt"):
+    df = read_csv(file, header=None, sep=',')
     df.columns = ["Genes", "above20x"]
-    df.to_csv(file)
-#WORKS NOW ! 
+    df.to_csv(file, index=False) 
 
 #Merge data frames by "Genes" using outer join------------------------------------------- 
 
 df = pd.DataFrame(columns=["Genes", "above20x"]) #Create empty database with which to merge
 
-
-
-
+for file in glob.glob("/home/rduffin/Desktop/R_projects/*new.txt"):
+    data = pd.read_csv(file)
+    df.merge(data, on = "Genes", how = 'outer')
+    
+df.to_csv('/home/rduffin/Desktop/R_projects/mergedfile.csv', index=False) #write merged df to csv
 #------------------------------------------------------------------------------------------
 
 #path = "/home/rduffin/Desktop/R_projects/*.csv"
