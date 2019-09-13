cef4792627470c9f2d836156dcd4ef692f9c884e
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-13 13:54:43 +0100

    Merges into one file with 2 columns

--- Merge.py
+++ Merge.py
@@ -7,20 +7,29 @@ import pandas as pd
 from pandas import read_csv
 
 import glob
-for file in glob.glob("/home/rduffin/Desktop/R_projects/*new.txt"):
+for file in glob.glob("/home/rduffin/Desktop/R_projects/*new.csv"):
     df = read_csv(file, header=None, sep=',')
     df.columns = ["Genes", "above20x"]
     df.to_csv(file, index=False) 
 
-#Merge data frames by "Genes" using outer join------------------------------------------- 
+
+#Merge data frames by "Genes" using outer join------------------------------------------- merges them into one file but only 2 columns (repeated gene names)
 
 df = pd.DataFrame(columns=["Genes", "above20x"]) #Create empty database with which to merge
+df.to_csv(r"/home/rduffin/Desktop/R_projects/merged.csv", index = None, header=True) #write df to csv
 
-for file in glob.glob("/home/rduffin/Desktop/R_projects/*new.txt"):
-    data = pd.read_csv(file)
-    df.merge(data, on = "Genes", how = 'outer')
+filenames = glob.glob("/home/rduffin/Desktop/R_projects/*new.csv")
+
+with open('merged.csv', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+  #  data = pd.read_csv(file)
+ #   merged = pd.merge(df, data, on = "Genes", how = 'outer')
     
-df.to_csv('/home/rduffin/Desktop/R_projects/mergedfile.csv', index=False) #write merged df to csv
+#df.to_csv('/home/rduffin/Desktop/R_projects/mergedfile.csv', index=False) #write merged df to csv
 #------------------------------------------------------------------------------------------
 
 #path = "/home/rduffin/Desktop/R_projects/*.csv"
