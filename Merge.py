c2466a5c95cbc01543ef5e9869a0b1cb91c52180
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-16 16:49:14 +0100

    End of Monday

--- Merge.py
+++ Merge.py
@@ -7,29 +7,27 @@ import numpy as np
 
 for file in glob.glob("/home/rduffin/Desktop/R_projects/*.chanjo_txt"):
     df = pd.read_csv(file, header = None, names=["Genes", "above20x", "average"], usecols=['Genes', "above20x"], sep = '\t') 
-    df ['patientID'] = file.split("_")[3]
+    df ['sampleID'] = file.split("_")[3]
     outfile = file.replace("chanjo_txt", "copy_chanjo_txt")
     df.to_csv(outfile, index=False) #write to CSV
 
-#Writes each row of each file to one file-------------------------------------------
+#Writes each row of each file to one file-------------------------------------------------------------------------------------------------------------
 
-df = pd.DataFrame(columns=["Genes", "above20x"]) #Create empty database with which to merge
-df.to_csv(r"/home/rduffin/Desktop/R_projects/merged.csv", index = None, header=True) #write df to csv
+filenames = glob.glob("/home/rduffin/Desktop/R_projects/*copy_chanjo_txt")
 
-filenames = glob.glob("/home/rduffin/Desktop/R_projects/*new.csv")
+li = []
 
-with open('merged.csv', 'w') as outfile:
-    for fname in filenames:
-        with open(fname) as infile:
-            for line in infile:
-                outfile.write(line)
+for filename in filenames:
+    df = pd.read_csv(filename, index_col=None)
+    li.append(df)
 
-  #  data = pd.read_csv(file)
- #   merged = pd.merge(df, data, on = "Genes", how = 'outer')
-    
-#df.to_csv('/home/rduffin/Desktop/R_projects/mergedfile.csv', index=False) #write merged df to csv
-#------------------------------------------------------------------------------------------
+merged = pd.concat(li, axis=0, ignore_index=True)
+merged.to_csv(r"/home/rduffin/Desktop/R_projects/merged.csv", index = None, header=True) #write df to csv
 
-#path = "/home/rduffin/Desktop/R_projects/*.csv"
+#Give new header names for genesymbols file-------------------------------------------------------------------------------------------------------------------
+db = pd.read_csv("genesymbols.csv", index_col=None) #Read genesymbols database
+new_header = ['Genes', 'Symbols'] 
+db.to_csv("genesymbolscopy.csv", index = None, header=new_header)
 
-#     
+
+#Merge the dataframe with the database gene symbols/Entrez IDs-----------------------------------------------------------------------------------------
