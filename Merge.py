5a695088a1bec0c6bffbed7cecb173121e1b5f79
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-19 15:22:54 +0100

    End of day

--- Merge.py
+++ Merge.py
@@ -6,14 +6,14 @@ import numpy as np
 #Prepare the genesymbols.csv file for merging-------------------------------------------------------------------------------------------------------------------
 
 db = pd.read_csv("genesymbols.csv", index_col=None) #Read genesymbols database
-db.columns=["Gene", "Symbols"] #Assign column names to db
+db.columns=["Entrez", "Gene"] #Assign column names to db
 
 #Prepare all patient data files for merging and concatenate------------------------------------------------------------------------------!
 
 li = []
 
 for file in glob.glob("/home/rduffin/Desktop/R_projects/*.chanjo_txt"): #creates a list of filenames to use in the loop
-    df = pd.read_csv(file, header = None, names=["Gene", "above20x", "average"], usecols=['Gene', "above20x"], delimiter='\t') #reads each csv file
+    df = pd.read_csv(file, header = None, names=["Entrez", "above20x", "average"], usecols=['Entrez', "above20x"], delimiter='\t') #reads each csv file
     df = df.astype('str') #converts dataframe from int to str 
     df ['Sample'] = file.split("_")[3] #splits file name by _ and adds third item from split to a new column named 'Sample' for each dataframe
     li.append(df) #appends each dataframe to li
@@ -22,6 +22,8 @@ merged = pd.concat(li, axis=0, ignore_index=True) #concatenates all dataframes i
 
 #Add gene symbols ready for use in the webapp-----------------------------------------------------------------------------------------
 
-new = pd.merge(merged, db, how="left", on="Gene") #Perform left merge on "Gene"
+#SOME OF THE GENE SYMBOLS JUST HAVE BLANK SPACES- look into what went wrong
+
+new = pd.merge(merged, db, how="left", on="Entrez") #Perform left merge on "Gene"
 final = new[['Sample', 'Gene', 'above20x']] #Create new dataframe with necessary columns in correct order
 final.to_csv("forboxplot.csv", index = False) #Write to a csv file ready for the app
