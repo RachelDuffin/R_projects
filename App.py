9b5de549a9b9ad53518d99b365dae573635b0ea1
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-20 13:40:22 +0100

    cant work out how to add boxplot

--- App.py
+++ App.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3.6
 
+import pandas as pd
+import numpy as np
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import plotly.graph_objs as go
 
-#Activate the Dash Server----------------------------------------
+#Activate the Dash Server---------------------------------------------------------------------------------------------------------
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -15,28 +16,33 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
     
-#Import the data------------------------------------------------------------
+#Import the data-------------------------------------------------------------------------------------------------------------------
 
 df = pd.read_csv("forboxplot.csv") #import csv
 
-#Create the app layout- This displays ordered dropdown menu of gene names-----------------
+#Create the app layout- This displays ordered dropdown menu of gene names----------------------------------------------------------
 
 app.layout = html.Div(children=[
     html.H4(children='Horizontal coverage for WES'), #Header
     dcc.Dropdown(
-        id='dropdown',
+        id='genechoice',
         options=[{'label': i, 'value': i} for i in sorted(df.Gene.unique())], #only lists each gene once (unique), and sorts them alphabetically
         multi=False, placeholder='Enter HGNC Gene Symbol:'), #Prevents simultaneous selection of genes, show "Filter by gene" in selection box
-    html.Div(id='dropdown-container')
+    dcc.Graph(id='boxplot'),
+    html.Div(id='children')
 ])
 
-@app.callback(Output('dropdown-container', 'children'),
-              [Input('dropdown', 'value')])
-def display_table(dropdown_value):
-    
+@app.callback(Output(component_id='boxplot', component_property='figure'),
+              [Input(component_id='genechoice', component_property='value')])
+def update_graph(dropdown_value):
+    if dropdown_value is None:
+        return "None selected"
 
-    dff = df[df.state.str.contains('|'.join(dropdown_value))]
-    return generate_table(dff)
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+#How do I get the boxplot on there?
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+#------------------------------------------------------------------------------------------------------------------------------------
