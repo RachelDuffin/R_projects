7ac93a03ba2464fd265ef792e08b0d4051d0a71c
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-24 15:43:51 +0100

    List of gene names created

--- Merge.py
+++ Merge.py
@@ -3,12 +3,12 @@ import os, sys, csv, glob, re, subprocess
 import pandas as pd
 import numpy as np
 
-#Prepare the genesymbols.csv file for merging-------------------------------------------------------------------------------------------------------------------
+#Prepare the genesymbols.csv file for merging----------------------------------------------------------------------------------------------------------------------------------
 
 db = pd.read_csv("genesymbols.csv", index_col=None) #Read genesymbols database
 db.columns=["Entrez", "Gene"] #Assign column names to db
 
-#Prepare all patient data files for merging and concatenate------------------------------------------------------------------------------!
+#Prepare all patient data files for merging and concatenate--------------------------------------------------------------------------------------------------------------------
 
 li = []
 
@@ -20,10 +20,18 @@ for file in glob.glob("/home/rduffin/Desktop/R_projects/*.chanjo_txt"): #creates
 
 merged = pd.concat(li, axis=0, ignore_index=True) #concatenates all dataframes in li  
 
-#Add gene symbols ready for use in the webapp-----------------------------------------------------------------------------------------
-
-#SOME OF THE GENE SYMBOLS JUST HAVE BLANK SPACES- look into what went wrong
+#Add gene symbols ready for use in the webapp, and remove all rows with Entrez IDs that have no gene symbols--------------------------------------------------------------------
 
 new = pd.merge(merged, db, how="left", on="Entrez") #Perform left merge on "Gene"
+new.replace('', np.nan, inplace=True) #Replace blank cells with NaN values
+new.dropna(subset=['Gene'], inplace=True) #Drop rows that contain NaN from data frame 
 final = new[['Sample', 'Gene', 'above20x']] #Create new dataframe with necessary columns in correct order
 final.to_csv("forboxplot.csv", index = False) #Write to a csv file ready for the app
+
+#Create list of gene names (no repeats) ----------------------------------------------------------------------------------------------------------------------------------------
+my_list = final["Gene"].values #Creates list of gene names from dataframe
+names = np.unique(my_list) #finds unique gene names and returns sorted elements
+np.savetxt("names.csv", names, fmt='%s') #Write numpy array to a csv file ready for the app
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
