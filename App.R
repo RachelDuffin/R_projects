e0cc2fbfb07e69b6c28a37c206f215b286e1c453
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-26 16:17:00 +0100

    All working!

--- App.R
+++ App.R
@@ -2,7 +2,6 @@ library(shiny)
 library(tidyverse)
 library(fivethirtyeight)
 library(plotly)
-library(org.Hs.eg.db)
 library(dplyr)
 
 #Import data------------------------------------------------------------------------------------------------
@@ -25,7 +24,7 @@ ui <- pageWithSidebar(
                 choices = genename, multiple = TRUE, options=list(placeholder ='Gene Symbol', maxOptions = 10000000, maxItems=1))),
   mainPanel(h3("% of bases above 20X"),
             plotOutput(outputId="myBoxplot"),
-            h4("Box plot shows 1st-3rd quartile, with the median value represented by a horizontal line. Outliers are defined as data points less than or greater than 1.5 times the interquartile range beyond the 1st and 3rd quartiles respectively, and are represented by dots. Whiskers show the range of inliers. Coverage calculated for RefSeq exonic bases +/- 5bp. N = 100 exomes (Agilent SureSelect Clinical Research Exome)."))
+            h6("Box plot shows 1st-3rd quartile, with the median value represented by a horizontal line. Outliers are defined as data points less than or greater than 1.5 times the interquartile range beyond the 1st and 3rd quartiles respectively, and are represented by dots. Whiskers show the range of inliers. Coverage calculated for RefSeq exonic bases +/- 5bp. N = 100 exomes (Agilent SureSelect Clinical Research Exome)."))
 
 )
   
@@ -34,12 +33,12 @@ ui <- pageWithSidebar(
 # Define server logic required to generate and plot boxplots
 server <- function(input, output) {
 
-  output$caption <- renderText({shiny::validate(input$selectgene)})
+  output$caption <- renderText({(input$selectgene)})
   
-  output$myBoxplot <- renderPlot({shiny::validate(dfsubset <- filter(df, Gene == input$selectgene), 
-  p <- (ggplot(dfsubset, aes(x='', y=above20x)) + geom_boxplot(aes(group = Gene))) +  coord_cartesian(ylim = c(0, 100)),
+  output$myBoxplot <- renderPlot({dfsubset <- filter(df, Gene == input$selectgene) 
+  p <- (ggplot(dfsubset, aes(x='', y=above20x)) + geom_boxplot(aes(group = Gene))) +  coord_cartesian(ylim = c(0, 100))
   print(p)
-  )})
+  }, height = 400, width = 800)
 }
 
 shinyApp(ui = ui, server = server)
