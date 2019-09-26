a49bb0c0bfbd29664641993901fccb3762453e46
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-26 15:42:39 +0100

    Up to date

--- App.R
+++ App.R
@@ -7,12 +7,9 @@ library(dplyr)
 
 #Import data------------------------------------------------------------------------------------------------
 setwd("/home/rduffin/Desktop/R_projects")
-df <- read.table("forboxplot.csv", header = TRUE, sep = ",") #create data table of collated data
+df <- read.table("forboxplot.txt", header = TRUE, sep = " ", row.names = NULL) #create data table of collated data
 #mydata <- mydata %>% mutate_all(as.character) #Convert to characters
-genename <- read.table("names.csv", header = TRUE) 
-
-
-#FOR SOME REASON APP IS ONLY SHOWING GENES STARTING WITH A AS AN OPTION
+genename <- read.table("names.txt", header = TRUE, row.names = NULL) 
 
 #UI -----------------------------------------------------------------------------------------------------------
 
@@ -24,8 +21,8 @@ ui <- pageWithSidebar(
   
   # Sidebar with controls to select dataset of specific gene
   sidebarPanel(
-    selectizeInput(inputId = "gene", label = "Enter HGNC gene symbol:", 
-                choices = genename, multiple = FALSE, options=list(placeholder ='Gene Symbol', maxOptions = 100000))),
+    selectizeInput(inputId = "selectgene", label = "Enter HGNC gene symbol:", 
+                choices = genename, multiple = TRUE, options=list(placeholder ='Gene Symbol', maxOptions = 10000000, maxItems=1))),
   mainPanel(h3("% of bases above 20X"),
             plotOutput(outputId="myBoxplot"),
             h4("Box plot shows 1st-3rd quartile, with the median value represented by a horizontal line. Outliers are defined as data points less than or greater than 1.5 times the interquartile range beyond the 1st and 3rd quartiles respectively, and are represented by dots. Whiskers show the range of inliers. Coverage calculated for RefSeq exonic bases +/- 5bp. N = 100 exomes (Agilent SureSelect Clinical Research Exome)."))
@@ -37,12 +34,12 @@ ui <- pageWithSidebar(
 # Define server logic required to generate and plot boxplots
 server <- function(input, output) {
 
-  output$caption <- renderText(input$gene)
+  output$caption <- renderText({shiny::validate(input$selectgene)})
   
-  output$myBoxplot <- renderPlot({dfsubset <- filter(df, Gene == input$gene) 
-  p <- (ggplot(dfsubset, aes(x='', y='above20x')) + geom_boxplot(aes(group = Gene))) + geom_point() 
+  output$myBoxplot <- renderPlot({shiny::validate(dfsubset <- filter(df, Gene == input$selectgene), 
+  p <- (ggplot(dfsubset, aes(x='', y=above20x)) + geom_boxplot(aes(group = Gene))) +  coord_cartesian(ylim = c(0, 100)),
   print(p)
-  })
+  )})
 }
 
 shinyApp(ui = ui, server = server)
