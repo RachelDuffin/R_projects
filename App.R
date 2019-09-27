ae900d1875d8b7224ee9aeb2f79072e32e93fd26
RachelDuffin <rachel.g.duffin@gmail.com>
2019-09-27 10:35:18 +0100

    Working but boxplot bunched up at bottom

--- App.R
+++ App.R
@@ -3,11 +3,12 @@ library(tidyverse)
 library(fivethirtyeight)
 library(plotly)
 library(dplyr)
+library(shinycssloaders)
+library(highcharter)
 
 #Import data------------------------------------------------------------------------------------------------
 setwd("/home/rduffin/Desktop/R_projects")
 df <- read.table("forboxplot.txt", header = TRUE, sep = " ", row.names = NULL) #create data table of collated data
-#mydata <- mydata %>% mutate_all(as.character) #Convert to characters
 genename <- read.table("names.txt", header = TRUE, row.names = NULL) 
 
 #UI -----------------------------------------------------------------------------------------------------------
@@ -21,24 +22,27 @@ ui <- pageWithSidebar(
   # Sidebar with controls to select dataset of specific gene
   sidebarPanel(
     selectizeInput(inputId = "selectgene", label = "Enter HGNC gene symbol:", 
-                choices = genename, multiple = TRUE, options=list(placeholder ='Gene Symbol', maxItems=1))),
+                   choices = NULL, multiple = FALSE)),
   mainPanel(h3("% of bases above 20X", align="center"), #aligns heading to centre
             plotOutput(outputId="myBoxplot"),
-            h6("Box plot shows 1st-3rd quartile, with the median value represented by a horizontal line. Outliers are defined as data points less than or greater than 1.5 times the interquartile range beyond the 1st and 3rd quartiles respectively, and are represented by dots. Whiskers show the range of inliers. Coverage calculated for RefSeq exonic bases +/- 5bp. N = 100 exomes (Agilent SureSelect Clinical Research Exome)."), width = 5)
-  #width 5 specifies width of panel so restricts caption width
-  )
-  
+            h6("Box plot shows 1st-3rd quartile, with the median value represented by a horizontal line. Outliers are defined as data points less than or greater than 1.5 times the interquartile range beyond the 1st and 3rd quartiles respectively, and are represented by dots. Whiskers show the range of inliers. Coverage calculated for RefSeq exonic bases +/- 5bp. N = 100 exomes (Agilent SureSelect Clinical Research Exome)."), 
+            width = 5) #width 5 specifies width of panel so restricts caption width
+)
+
 #Server---------------------------------------
 
 # Define server logic required to generate and plot boxplots
-server <- function(input, output) {
-
+server <- function(input, output, session) {
+  
+  updateSelectizeInput(session = session, inputId = 'selectgene', options=list(placeholder= 'Gene name...', maxItems=1), choices = c('', genename), server = TRUE) #stores the genenames server side so it isn't too slow
+  
   output$caption <- renderText({(input$selectgene)})
   
-  output$myBoxplot <- renderPlot({dfsubset <- filter(df, Gene == input$selectgene) 
-  p <- (ggplot(dfsubset, aes(x='', y=above20x)) + geom_boxplot(aes(group = Gene))) +  coord_cartesian(ylim = c(0, 100))
-  print(p)
-  }, height = 400, width = 700) #specifies size of boxplot so it doesn't fit to page
+  output$myBoxplot <- renderPlot({
+    dfsubset <- filter(df, Gene == input$selectgene) 
+    p <- (ggplot(dfsubset, aes(x='', y=above20x)) + geom_boxplot(aes(group = Gene))) +  coord_cartesian(ylim = c(0, 100))
+    print(p)
+    }) #specifies size of boxplot so it doesn't fit to page
 }
 
 shinyApp(ui = ui, server = server)
